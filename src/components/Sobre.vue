a8c2ba3c9ce33d5a4a24e29dfdd5d3fcf667001b
Flavio Santana <f.o.santana@gmail.com>
2020-04-23 07:57:44 -0300

    entendendo como funcionado os components dinâmicos

--- src/components/Sobre.vue
+++ src/components/Sobre.vue
@@ -1,3 +1,14 @@
 <template>
     <h2>Sobre...</h2>
 </template>
+
+<script>
+export default {
+    created(){
+        console.log('created')
+    },
+    destroyed(){
+        console.log('destroyed')
+    }
+}
+</script>
